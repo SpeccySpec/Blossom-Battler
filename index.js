94a59a908a18f1d34187bfcc2f7d8d5a8ce2ce3f
Felix44 <87673997+Felix-44@users.noreply.github.com>
2022-03-27 18:17:05 +0200

    fix prefix not being cut off

--- index.js
+++ index.js
@@ -292,9 +292,11 @@ commands.test = new Command({
 	}
 })
 
+const prefix = "rpg!"
+
 client.on("messageCreate", (message) => {
-	if (!message.content.startsWith("rpg!")) return;
-	let args = [...message.content.matchAll(/"([^"]*?)"|[^ ]+/gm)].map(el => el[1] || el[0] || "")
+	if (!message.content.startsWith(prefix)) return;
+	let args = [...message.content.slice(prefix.length()).matchAll(/"([^"]*?)"|[^ ]+/gm)].map(el => el[1] || el[0] || "")
 	let command = commands[args.shift()]
 	if (!command) {
 		message.channel.send("That command does not exist!")
