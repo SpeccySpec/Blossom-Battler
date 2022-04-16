5a1c53771718bca2b24537b1b365b45699b8b794
Felix44 <felice.dangelo2013@gmail.com>
2022-04-16 14:55:04 +0200

    improve getCurrentDate function

--- client.js
+++ client.js
@@ -544,32 +544,27 @@ listArray = async(channel, theArray, page) => {
 	})
 }
 
+specialDates = {
+	"24 12": "Christmas Eve",
+	"25 12": "Christmas",
+	"26 12": "Boxing Day",
+	"31 12": "New Years' Eve",
+	"1 1": "New Years",
+	"1 4": "April Fools' day",
+	"2 6": "<@516359709779820544>'s birthday",
+	"31 10": "Halloween"
+}
+
 getCurrentDate = () => {
 	let today = new Date();
 	let dd = String(today.getDate()).padStart(2, '0');
 	let mm = String(today.getMonth() + 1).padStart(2, '0');
 	let yyyy = today.getFullYear();
 
-	today = mm + '/' + dd + '/' + yyyy;
+	today = specialDates[`${dd} ${mm}`] ?? mm + '/' + dd + '/' + yyyy;
 
-	if (mm === '12' && dd === '24')
-		today = 'Christmas Eve';
-	else if (mm === '12' && dd === '25')
-		today = 'Christmas';
-	else if (mm === '12' && dd === '26')
-		today = 'Boxing Day';
-	else if (mm === '12' && dd === '31')
-		today = "New Years' Eve";
-	else if (mm === '1' && dd === '1')
-		today = 'New Years';
-	else if (mm === '4' && dd === '1')
-		today = "April Fools' day";
-	else if (mm === '4' && dd === '17' && yyyy == '2022')
+	if (dd === '17' && mm === '4' && yyyy == '2022')
 		today = 'Easter (2022)';
-	else if (mm === '6' && dd === '2')
-		today = "<@516359709779820544>'s birthday";
-	else if (mm === '10' && dd === '31')
-		today = 'Halloween';
 	
 	return today
 }
