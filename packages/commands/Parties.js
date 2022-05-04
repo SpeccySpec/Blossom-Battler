c1b640e25aaa09a081d6163dde3d0cc634b0116d
Spectra <68280729+SpeccySpec@users.noreply.github.com>
2022-05-04 18:40:57 +0100

    setleader

--- packages/commands/Parties.js
+++ packages/commands/Parties.js
@@ -60,7 +60,52 @@ commands.makeparty = new Command({
 			}
 		}
 
-		fs.writeFileSync(`${dataPath}/json/${message.guild.id}/parties.json`, JSON.stringify(charFile, null, '    '));
-		message.channel.send({content: `${args[0]} has been assembled! You can now use them in battles!`, embeds: [briefDescription(charDefs)]});
+		fs.writeFileSync(`${dataPath}/json/${message.guild.id}/parties.json`, JSON.stringify(parties, null, '    '));
+		message.channel.send(`${args[0]} has been assembled! You can now use them in battles!`);
+	}
+})
+
+commands.setleader = new Command({
+	desc: "Sets a character as the party's leader.",
+	aliases: ['partymake', 'maketeam', 'teammake', 'registerparty', 'registerteam'],
+	section: "parties",
+	checkban: true,
+	args: [
+		{
+			name: "Party Name",
+			type: "Word",
+			forced: true
+		},
+		{
+			name: "Character",
+			type: "Num",
+			forced: true
+		},
+	],
+	func: (message, args) => {
+		let settings = setUpSettings(message.guild.id);
+		let parties = setUpFile(`${dataPath}/json/${message.guild.id}/parties.json`);
+		let charFile = setUpFile(`${dataPath}/json/${message.guild.id}/characters.json`);
+
+		if (!parties[args[0]]) return message.channel.send(`${args[0]} is an invalid party!`);
+		if (!charFile[args[1]]) return message.channel.send(`${args[1]} is an invalid character!`);
+
+		if (parties[args[0]] && !utilityFuncs.RPGBotAdmin(message.author.id)) {
+			if (!isPartyLeader(message.author, parties[args[0]], message.guild.id)) return message.channel.send("You cannot edit this party.")
+		}
+
+		let inParty = false;
+		for (let i in parties[args[0]].members) {
+			if (charFile[parties[args[0]].members[i]] && parties[args[0]].members[i].toLowerCase() == args[1].toLowerCase()) {
+				inParty = true;
+				parties[args[0]].members.splice(i, 1);
+			}
+		}
+
+		if (!inParty) return message.channel.send(`${args[1]} is not in the party!`);
+		parties[args[0]].members.unshift(args[1]);
+
+		fs.writeFileSync(`${dataPath}/json/${message.guild.id}/parties.json`, JSON.stringify(parties, null, '    '));
+		message.channel.send(`${charFile[args[1]]} is now the leader of ${args[0]}!`);
 	}
 })
