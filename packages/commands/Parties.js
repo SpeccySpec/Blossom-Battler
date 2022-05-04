de9f438cb203f14360b3086259826a0219629434
Spectra <68280729+SpeccySpec@users.noreply.github.com>
2022-05-04 20:34:11 +0100

    more party shit

--- packages/commands/Parties.js
+++ packages/commands/Parties.js
@@ -9,6 +9,32 @@ isPartyLeader = (user, party, guild) => {
 	return user.id === partyLeader(party);
 }
 
+partyNum = (char, party) => {
+	for (const i in party.members) {
+		if (party.members[i] == char) return i;
+	}
+
+	return null;
+}
+
+backupPartyNum = (char, party) => {
+	for (const i in party.backup) {
+		if (party.backup[i] == char) return i;
+	}
+
+	return null;
+}
+
+removeFromParty = (party, char) => {
+	let pnum = partyNum(char, party);
+
+	if (pnum) {
+		party.members.splice(partyNum(char, party), 1);
+		return true;
+	} else
+		return false;
+}
+
 commands.makeparty = new Command({
 	desc: 'Makes a team to battle with! They can consist of up to 4 members in a main group, and an unlimited amount in backup by default.',
 	aliases: ['partymake', 'maketeam', 'teammake', 'registerparty', 'registerteam'],
@@ -22,10 +48,10 @@ commands.makeparty = new Command({
 		},
 		{
 			name: "Characters",
-			type: "Num",
+			type: "Word",
 			forced: true,
 			multiple: true
-		},
+		}
 	],
 	func: (message, args) => {
 		let settings = setUpSettings(message.guild.id);
@@ -46,7 +72,9 @@ commands.makeparty = new Command({
 			backup: [],
 			items: {},
 			weapons: {},
-			armors: {}
+			armors: {},
+			negotiates: {},
+			negotiateAllies: {}
 		}
 		
 		for (let i in args) {
@@ -67,7 +95,7 @@ commands.makeparty = new Command({
 
 commands.setleader = new Command({
 	desc: "Sets a character as the party's leader.",
-	aliases: ['partymake', 'maketeam', 'teammake', 'registerparty', 'registerteam'],
+	aliases: ['partyleader', 'leader', 'makeleader'],
 	section: "parties",
 	checkban: true,
 	args: [
@@ -78,9 +106,9 @@ commands.setleader = new Command({
 		},
 		{
 			name: "Character",
-			type: "Num",
+			type: "Word",
 			forced: true
-		},
+		}
 	],
 	func: (message, args) => {
 		let settings = setUpSettings(message.guild.id);
@@ -94,18 +122,160 @@ commands.setleader = new Command({
 			if (!isPartyLeader(message.author, parties[args[0]], message.guild.id)) return message.channel.send("You cannot edit this party.")
 		}
 
-		let inParty = false;
-		for (let i in parties[args[0]].members) {
-			if (charFile[parties[args[0]].members[i]] && parties[args[0]].members[i].toLowerCase() == args[1].toLowerCase()) {
-				inParty = true;
-				parties[args[0]].members.splice(i, 1);
-			}
+		if (removeFromParty(parties[args[0]], args[1])) {
+			parties[args[0]].members.unshift(args[1]);
+			fs.writeFileSync(`${dataPath}/json/${message.guild.id}/parties.json`, JSON.stringify(parties, null, '    '));
+			message.channel.send(`${charFile[args[1]]} is now the leader of ${args[0]}!`);
+		} else {
+			return message.channel.send(`${args[1]} is not in the party!`);
+		}
+	}
+})
+
+commands.changepos = new Command({
+	desc: "Changes a characters' position in the party.",
+	aliases: ['partyposition', 'position', 'partyorder'],
+	section: "parties",
+	checkban: true,
+	args: [
+		{
+			name: "Party Name",
+			type: "Word",
+			forced: true
+		},
+		{
+			name: "Character",
+			type: "Word",
+			forced: true
+		},
+		{
+			name: "Position",
+			type: "Num",
+			forced: true
+		}
+	],
+	func: (message, args) => {
+		let settings = setUpSettings(message.guild.id);
+		let parties = setUpFile(`${dataPath}/json/${message.guild.id}/parties.json`);
+		let charFile = setUpFile(`${dataPath}/json/${message.guild.id}/characters.json`);
+
+		if (!parties[args[0]]) return message.channel.send(`${args[0]} is an invalid party!`);
+		if (!charFile[args[1]]) return message.channel.send(`${args[1]} is an invalid character!`);
+
+		if (parties[args[0]] && !utilityFuncs.RPGBotAdmin(message.author.id)) {
+			if (!isPartyLeader(message.author, parties[args[0]], message.guild.id)) return message.channel.send("You cannot edit this party.")
 		}
 
-		if (!inParty) return message.channel.send(`${args[1]} is not in the party!`);
-		parties[args[0]].members.unshift(args[1]);
+		if (removeFromParty(parties[args[0]], args[1])) {
+			parties[args[0]].members.splice(args[2], 0, args[1]);
+			fs.writeFileSync(`${dataPath}/json/${message.guild.id}/parties.json`, JSON.stringify(parties, null, '    '));
+			message.channel.send(`${charFile[args[1]]} is now the leader of ${args[0]}!`);
+		} else {
+			return message.channel.send(`${args[1]} is not in the party!`);
+		}
+	}
+})
 
+commands.backup = new Command({
+	desc: "Makes a character in the party backup and vice versa.",
+	aliases: ['makebackup', 'swapbackup'],
+	section: "parties",
+	checkban: true,
+	args: [
+		{
+			name: "Party Name",
+			type: "Word",
+			forced: true
+		},
+		{
+			name: "Character",
+			type: "Word",
+			forced: true
+		}
+	],
+	func: (message, args) => {
+		let settings = setUpSettings(message.guild.id);
+		let parties = setUpFile(`${dataPath}/json/${message.guild.id}/parties.json`);
+		let charFile = setUpFile(`${dataPath}/json/${message.guild.id}/characters.json`);
+
+		if (!parties[args[0]]) return message.channel.send(`${args[0]} is an invalid party!`);
+		if (!charFile[args[1]]) return message.channel.send(`${args[1]} is an invalid character!`);
+
+		if (parties[args[0]] && !utilityFuncs.RPGBotAdmin(message.author.id)) {
+			if (!isPartyLeader(message.author, parties[args[0]], message.guild.id)) return message.channel.send("You cannot edit this party.")
+		}
+
+		let pnum = partyNum(args[1], parties[args[0]]);
+		let bnum = backupPartyNum(args[1], parties[args[0]]);
+
+		if (pnum) {
+			parties[args[0]].members.splice(pnum, 1);
+			parties[args[0]].backup.push(args[1]);
+		} else if (bnum) {
+			parties[args[0]].backup.splice(bnum, 1);
+			parties[args[0]].members.push(args[1]);
+		} else {
+			return message.channel.send(`${args[1]} is not in the party!`);
+		}
+
+		message.react('👍');
 		fs.writeFileSync(`${dataPath}/json/${message.guild.id}/parties.json`, JSON.stringify(parties, null, '    '));
-		message.channel.send(`${charFile[args[1]]} is now the leader of ${args[0]}!`);
+	}
+})
+
+commands.getparty = new Command({
+	desc: "Get Party Info, including rings, pets, weapons and more!",
+	aliases: ['partyinfo'],
+	section: "parties",
+	checkban: true,
+	args: [
+		{
+			name: "Party Name",
+			type: "Word",
+			forced: true
+		}
+	],
+	func: (message, args) => {
+		let settings = setUpSettings(message.guild.id);
+		let parties = setUpFile(`${dataPath}/json/${message.guild.id}/parties.json`);
+
+		if (!parties[args[0]]) return message.channel.send(`${args[0]} is an invalid party!`);
+
+		// Alright, let's get the party!
+		let DiscordEmbed = partyDesc(parties[args[0]], message);
+		message.channel.send({embeds: [DiscordEmbed]});
+	}
+})
+
+commands.listparty = new Command({
+	desc: "Get Party Info, including rings, pets, weapons and more!",
+	aliases: ['listparties', 'listteam', 'listteams'],
+	section: "parties",
+	checkban: true,
+	args: [
+		{
+			name: "Leader",
+			type: "Ping",
+			forced: false
+		}
+	],
+	func: (message, args) => {
+		let settings = setUpSettings(message.guild.id);
+		let parties = setUpFile(`${dataPath}/json/${message.guild.id}/parties.json`);
+
+		let array = [];
+		for (let i in parties) {
+			let members = '';
+			
+			for (const k in parties[i].members) {
+				members += parties[i].members[k];
+				if (k <= parties[i].members.length) members += ', ';
+			}
+
+			array.push({title: i, desc: descTxt});
+		}
+
+		// Alright, let's get the party!
+		listArray(message.channel, array, message.author.id);
 	}
 })
---
+++ packages/party/partyInfo.js
@@ -0,0 +1,55 @@
+partyDesc = (party, message) = {
+	let m = '';
+	for (const i in party.members) m += `\n${party.members[i]}`;
+	if (m === '') m = 'Empty...';
+
+	let b = '';
+	for (const i in party.backup) b += `\n${party.backup[i]}`;
+	if (b === '') b = 'No backup.';
+
+	let p = '';
+	for (const i in party.negotiateAllies) {
+		let petDefs = party.negotiateAllies[i]
+		p += `\n${petDefs.name} - ${petDefs.skill}`;
+	}
+
+	if (p === '') p = 'No backup.';
+
+	// Items
+	let items = '';
+	for (const i in party.items) items += `i: ${party.items[i]}\n`;
+	if (items === '') items = 'No items.';
+
+	// Weapons and Armor
+	let weapons = '';
+	let armor = '';
+
+	if (party.weapons) {
+		for (const i in party.weapons) {
+			let weaponDefs = party.weapons[i]
+			weapons += `${i} - **${weaponDefs.atk ? weaponDefs.atk : '0'}ATK**, **${weaponDefs.mag ? weaponDefs.mag : '0'}MAG**\n`;
+		}
+	}
+
+	if (party.armors) {
+		for (const i in party.armors) {
+			let armorDefs = party.armors[i]
+			armor += `${i} - **${armorDefs.def ? armorDefs.def : '0'}DEF**\n`;
+		}
+	}
+
+	if (weapons === '') weapons = 'No weapons.';
+	if (armor === '') armor = 'No armor.';
+
+	return new Discord.MessageEmbed()
+		.setColor('#e36b2b')
+		.setTitle(`Team ${arg[1]}`)
+		.addFields(
+			{ name: 'Members', value: `${m}\n\n**${party.currency} ${settings.currency}s**`, inline: true },
+			{ name: 'Items', value: items, inline: true },
+			{ name: 'Backup', value: b, inline: true },
+			{ name: 'Pets', value: p, inline: true },
+			{ name: 'Weapons', value: weapons, inline: true },
+			{ name: 'Armors', value: armor, inline: true }
+		)
+}
