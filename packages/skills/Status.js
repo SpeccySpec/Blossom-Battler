94866f68c09ee06a4aefc6f37da2fd26067c99b8
Spectra <68280729+SpeccySpec@users.noreply.github.com>
2022-06-27 05:42:04 +0100

    PacifyStatus Status

--- packages/skills/Status.js
+++ packages/skills/Status.js
@@ -771,12 +771,68 @@ statusList = {
 
 			makeStatus(skill, "pacifystatus", [status, amount]);
 			return true;
+		},
+		onuse(char, targ, skill, btl, vars) {
+			if (!targ.enemy) return 'But it failed!';
+
+			if ((vars[0] === 'all' && char.status) || (vars[0] === 'mental' && !isPhysicalStatus(char.status?)) || (vars[0] === 'physical' && isPhysicalStatus(char.status?)) || char.status? === vars[0]) {
+				let finaltxt = `${targ.name} was pacified by ${skill.name}, by ${vars[1]}%.`;
+				targ.pacify += vars[1];
+
+				if (targ.pacify >= 100) {
+					targ.pacified = true;
+					if (targ.negotiateDefs) {
+						let parties = setUpFile(`${dataPath}/json/${btl.guild.id}/parties.json`, true);
+
+						if (parties[btl.teams[char.team].id]) {
+							let party = parties[btl.teams[char.team].id];
+
+							if (!party.negotiates) party.negotiates = {};
+							party.negotiates[targ.name] = party.negotiates[targ.name] ? party.negotiates[targ.name]+1 : 1
+
+							if (party.negotiates[targ.name] == targ.negotiateDefs.required) {
+								finaltxt += 'and wants to join your team!';
+
+								party.negotiateAllies[targ.name] = {
+									nickname: targ.name,
+									hp: Math.round(targ.hp/2),
+									mp: Math.round(targ.mp/2),
+									maxhp: Math.round(targ.maxhp/2),
+									maxmp: Math.round(targ.maxmp/2),
+									stats: targ.stats,
+
+									skill: targ.negotiateDefs.qualities.skill,
+									atkbuff: targ.negotiateDefs.qualities.atk,
+									magbuff: targ.negotiateDefs.qualities.mag,
+									endbuff: targ.negotiateDefs.qualities.end,
+
+									happines: 255, // OKAY BUT WHAT IF WE COULD DO THIS TAMAGOCHI THING WITH PETS THATD BE SO SICK
+									mood: 'happy', // YOU'D GET TO SEE THEIR MOOD AND SHIT
+									food: 100, // AND FEED THEM
+									// Although there wouldn't be no real punishment, maybe just a boost in damage output.
+									// Things like being forced to tank Makarakarn and Tetrakarn before would now lower happiness or mood ect
+								}
+							} else {
+								finaltxt += `and is satisfied!\n\n_(**${party.negotiates[targ.name]}/${targ.negotiateDefs.required}** ${targ.name}s pacified.)_`;
+							}
+
+							fs.writeFileSync(`${dataPath}/json/${btl.guild.id}/parties.json`, JSON.stringify(parties, null, '    '));
+						}
+					} else {
+						finaltxt += 'and stops attacking!';
+					}
+				}
+
+				return finaltxt;
+			} else {
+				return 'But it failed!';
+			}
 		}
 	}),
 
 	ragesoul: new Extra({
 		name: "Rage Soul",
-		desc: "Multiplies the user's Melee Attack Power by <Melee Power Multiplier> and their Attack Stat by <ATK Stat Multiplier>, but locks them into using Melee Attacks.",
+		desc: "Multiplies the user's Melee Attack Power by <Melee Power Multiplier> and their Attack Stat by <ATK Stat Multiplier>, but locks them into using Melee Attacks. If <Turns> is set to -1, this will be indefinite.",
 		args: [
 			{
 				name: "Melee Power Multiplier",
