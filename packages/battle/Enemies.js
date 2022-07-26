69bf2e1f34dee74219cac8df93430418049fdf56
Spectra <68280729+SpeccySpec@users.noreply.github.com>
2022-07-26 13:44:06 +0100

    Hard mode AI able to buff and use dekunda skills

--- packages/battle/Enemies.js
+++ packages/battle/Enemies.js
@@ -182,6 +182,85 @@ enemyThinker = (char, btl) => {
 										act.points += 5+Math.round(st[status]/statusses.length);
 									}
 								}
+								
+								// Alright time for other shit
+								if (skill.statusses) {
+									// Buffs
+									if (skill.statusses.buff && char.hp >= 2*char.maxhp/3) {
+										for (let vars of skill.statusses.buff) {
+											if (vars[0] === 'user') {
+												if (vars[2] < 0) 
+													act.points--;
+												else {
+													if (char.buffs[vars[1]]+vars[2] >= 3) {
+														act.points -= 3;
+													} else {
+														act.points += 3-char.buffs[vars[1]];
+													}
+												}
+											} else {
+												if (skill.target === 'ally' || skill.target === 'spreadallies') {
+													if (vars[2] < 0) 
+														act.points -= 2;
+													else {
+														if (targ.buffs[vars[1]]+vars[2] >= 3) {
+															act.points -= 3;
+														} else {
+															act.points += 3-targ.buffs[vars[1]];
+														}
+													}
+												} else if (skill.target === 'allallies') {
+													if (vars[2] < 0) 
+														act.points -= btl.teams[char.team].length*2;
+													else {
+														for (let ally of btl.teams[char.team]) {
+															if (ally.buffs[vars[1]]+vars[2] >= 3) {
+																act.points -= 3;
+															} else {
+																act.points += 3-ally.buffs[vars[1]];
+															}
+														}
+													}
+												} else {
+													if (vars[2] > 0) 
+														act.points -= 4;
+													else {
+														for (let ally of btl.teams[char.team]) {
+															if (ally.buffs[vars[1]]+vars[2] >= 3) {
+																act.points -= 3;
+															} else {
+																act.points += 3-ally.buffs[vars[1]];
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+									
+									// Dekunda
+									if (skill.statusses.dekunda) {
+										switch(skill.target) {
+											case 'allopposing':
+												for (let team of btl.teams) {
+													if (team === char.team) continue;
+													
+													for (let opp of team.members) {
+														for (let i in opp.buffs) {
+															if (opp.buffs[i] > 0) act.points += opp.buffs[i];
+														}
+													}
+												}
+												break;
+
+											case 'one':
+												for (let i in targ.buffs) {
+													if (targ.buffs[i] > 0) act.points += targ.buffs[i];
+												}
+												break;
+										}
+									}
+								}
 
 								break;
 
