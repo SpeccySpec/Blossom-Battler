165c406226257029eb740db13e05b8efa5ca8865
Spectra <68280729+SpeccySpec@users.noreply.github.com>
2022-08-01 15:01:27 +0100

    ForveMove not accepting melee

--- packages/skills/Extras.js
+++ packages/skills/Extras.js
@@ -1649,10 +1649,36 @@ killVar = (char, name) => {
 customVariables = {
 	forcemove: {
 		onturn(btl, char, vars) {
-			if (vars[2]) 
-				useSkill(char, btl, vars[1], vars[2]);
-			else
-				useSkill(char, btl, vars[1]);
+			if (vars[1].move === "melee") {
+				let atkType = 'physical';
+				let targType = 'one';
+				for (let skillName of char.skills) {
+					let psv = skillFile[skillName];
+					if (psv.type != 'passive' || !psv.passive) continue;
+
+					if (psv.passive.magicmelee) atkType = 'magic';
+					if (psv.passive.attackall) targType = 'allopposing';
+				}
+
+				let meleeAtk = {
+					name: char.melee.name,
+					type: char.melee.type,
+					pow: char.melee.pow,
+					acc: Math.min(100, char.melee.acc),
+					crit: char.melee.crit,
+					atktype: atkType,
+					target: 'one',
+					melee: true
+				}
+
+				useSkill(char, btl, vars[1], meleeAtk);
+				break;
+			} else {
+				if (vars[2])
+					useSkill(char, btl, vars[1], vars[2]);
+				else
+					useSkill(char, btl, vars[1]);
+			}
 
 			vars[0]--;
 			if (vars[0] == 0) killVar(char, "forcemove");
