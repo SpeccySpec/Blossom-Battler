63ca4617cbfeff7f3d27fd3b9a2a688f92e3f50a
Spectra <68280729+SpeccySpec@users.noreply.github.com>
2022-08-04 13:08:18 +0100

    oops

--- packages/skills/Passive.js
+++ packages/skills/Passive.js
@@ -1355,7 +1355,7 @@ passiveList = {
 		getinfo(vars, skill) {
 			return `Nets a **__${vars[1]}__** for every heal the user obtains until it reaches **${vars[0]}**`
 		}
-	})
+	}),
 
 /*
 	arenatrap: new Extra({
