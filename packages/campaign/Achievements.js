792a8169a6701c8ff0d511afe81a5f85ce0ea8e7
Spectra <68280729+SpeccySpec@users.noreply.github.com>
2022-08-06 12:28:13 +0100

    Achievement definitions, add more if you want

---
+++ packages/campaign/Achievements.js
@@ -0,0 +1,23 @@
+bbAchievements = [
+	// First achievements
+	{name: "New Beginings", stars: 1, desc: "Create your first character!", section: "beginings"},
+	{name: "First Encounter...!", stars: 1, desc: "Encounter your first enemy!", section: "beginings"},
+
+	// Murder achievements
+	{name: "Murder, murder!", stars: 1, desc: "Defeat 1 enemy.", section: "battle"},
+	{name: "That was tough...", stars: 1, desc: "Defeat 50 enemies", section: "battle"},
+	{name: "Getting better!", stars: 1, desc: "Defeat 100 enemies", section: "battle"},
+	{name: "Onslaught!", stars: 1, desc: "Defeat 200 enemies", section: "battle"},
+	{name: "There's no escape!", stars: 1, desc: "Defeat 350 enemies", section: "battle"},
+	{name: "Stabby stabby!", stars: 1, desc: "Defeat 500 enemies", section: "battle"},
+	{name: "Die.", stars: 1, desc: "Defeat 1000 enemies", section: "battle"},
+
+	// Friendship achievements
+	{name: "My first friend!", stars: 1, desc: "Pacify 1 enemy.", section: "battle"},
+	{name: "Adorable...", stars: 2, desc: "Pacify 50 enemies", section: "battle"},
+	{name: "So cute!", stars: 3, desc: "Pacify 100 enemies", section: "battle"},
+	{name: "So nice...", stars: 5, desc: "Pacify 200 enemies", section: "battle"},
+	{name: "...when you get to know them!", stars: 7, desc: "Pacify 350 enemies", section: "battle"},
+	{name: "Good friends!", stars: 8, desc: "Pacify 500 enemies", section: "battle"},
+	{name: "Best friends!", stars: 10, desc: "Pacify 1000 enemies", section: "battle"},
+]
