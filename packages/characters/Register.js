b38021f0787cb2e5b6172aaebaec94fd3e208c91
Verwex <34553484+Verwex@users.noreply.github.com>
2022-08-15 20:13:05 +0200

    show said emojis when viewing characters

--- packages/characters/Register.js
+++ packages/characters/Register.js
@@ -202,12 +202,12 @@ longDescription = (charDefs, level, server, message) => {
 	let desc = ''
 	if (char.weaponclass) {
 		if (typeof(char.weaponclass) == 'object') {
-			desc += `_Can wield ${char.weaponclass[0]} and ${char.weaponclass[1]} weapons._\n`;
+			desc += `_Can wield ${classEmoji.weapon[char.weaponclass[0]]}${char.weaponclass[0]} and ${classEmoji.weapon[char.weaponclass[1]]}${char.weaponclass[1]} weapons._\n`;
 		} else {
-			if (char.weaponclass && char.weaponclass != "none") desc += `_Can wield ${char.weaponclass} weapons._\n`;
+			if (char.weaponclass && char.weaponclass != "none") desc += `_Can wield ${classEmoji.weapon[char.weaponclass]}${char.weaponclass} weapons._\n`;
 		}
 	}
-	if (char.armorclass && char.armorclass != "none") desc += `_Can wear ${char.armorclass} armor._\n`;
+	if (char.armorclass && char.armorclass != "none") desc += `_Can wear ${classEmoji.weapon[char.armorclass]}${char.armorclass} armor._\n`;
 	desc += '\n';
 
 	if (char.leaderskill && settings.mechanics.leaderskills) desc += `**${[char.leaderskill.name.toUpperCase()]}**\n_${leaderSkillTxt[char.leaderskill.type]}_\n${char.leaderskill.var2}${(usesPercent[char.leaderskill.type] == true) ? '%' : ''} ${char.leaderskill.type} ${char.leaderskill.var1 ? `toward ${elementEmoji[char.leaderskill.var1] ?? ''}${char.leaderskill.var1.toUpperCase()}` : ''}`;
