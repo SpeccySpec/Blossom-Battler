1bd153b354ab5f104de1eae5fe21d68bb15a7b04
Verwex <34553484+Verwex@users.noreply.github.com>
2022-08-16 13:13:03 +0200

    all heal skills to work with all boosts

--- packages/battle/Using Skills.js
+++ packages/battle/Using Skills.js
@@ -319,18 +319,16 @@ attackWithSkill = (char, targ, skill, btl, noRepel) => {
 
 			if (char.mimic || char.clone || char.reincarnate) skill.pow *= 1/3;
 
-			skill.pow = Math.round(skill.pow);
-
 			for (let i in skill.heal) {
 				if (!healList[i]) continue;
 				if (!healList[i].onuse) continue;
 
 				if (healList[i].multiple) {
 					for (let k in skill.heal[i]) {
-						result.txt += `\n${healList[i].onuse(char, targ, skill, btl, skill.heal[i][k]) ?? ''}`;
+						result.txt += `\n${healList[i].onuse(char, targ, skill, btl, skill.heal[i][k], skill.pow) ?? ''}`;
 					}
 				} else {
-					result.txt += `\n${healList[i].onuse(char, targ, skill, btl, skill.heal[i]) ?? ''}`;
+					result.txt += `\n${healList[i].onuse(char, targ, skill, btl, skill.heal[i], skill.pow) ?? ''}`;
 				}
 			}
 
@@ -987,6 +985,7 @@ useSkill = (char, btl, act, forceskill, ally) => {
 	}
 
 	// First, we modify stats via passives n shit. This isn't the actual character anyway so we don't care.
+	if (skill.type === 'heal') skill.pow = 1; //this is to make sure healing skills can be modified by passives
 
 	// Failsafe
 	if (!skill.hits) skill.hits = 1;
