c1185dc23e362de21d56314fbd133126d22d240c
Maiori44 <felice.dangelo2013@gmail.com>
2024-04-13 10:02:04 +0200

    we wont need that anymore

--- packages/backups.js
+++ packages/backups.js
@@ -11,16 +11,5 @@ exports.SaveBackup = async function() {
 		const backups = fs.readdirSync("backups").length
 
 		if (backups > 10) exec("cd backups && rm -r \"$(ls -t | tail -1)\"")
-
-		// Update backups automatically.
-/*		setTimeout(() => {
-			exec('git add backups', (error, stdout, stderr) => {
-				if (error) return void console.log(stderr);
-				exec('git commit -m "Update backups (AUTOMATED BY BB)"', (error, stdout, stderr) => {
-					if (error) return void console.log(stderr);
-					exec("git push");
-				})
-			})
-		}, 10000)*/
 	})
 }
