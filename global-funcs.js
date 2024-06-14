7bf8303b2c985ccdc9b9aee35bd8d78d488ea93a
Spectra <68280729+SpeccySpec@users.noreply.github.com>
2024-06-14 07:52:22 +0100

    f

--- global-funcs.js
+++ global-funcs.js
@@ -425,7 +425,7 @@ statusEmojis = {
 }
 
 elementTechs = {
-	burn: ['explode', 'wind', 'earth', 'nuclear'],
+	burn: ['explode', 'wind', 'grass', 'nuclear'],
 	bleed: ['slash', 'pierce', 'nuclear'],
 	freeze: ['strike', 'explode', 'fire', 'earth', 'gravity', 'nuclear', 'metal'],
 	stun: ['strike', 'earth', 'metal', 'electric'],
